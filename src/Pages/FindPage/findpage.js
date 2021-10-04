1b036601af4b95aa31231527ec6a7ecf301af786
ShrutiOmer <shrutiomer1234@gmail.com>
2021-10-04 12:37:49 +0530

    page added

--- src/Pages/FindPage/findpage.js
+++ src/Pages/FindPage/findpage.js
@@ -3,24 +3,25 @@ import Navbar from '../../Components/Navbar/Navbar';
 import hostel from '../../Assets/images/hostel.jpg';
 import styles from './findpage.module.css';
 import Drop2down from './Drop2down';
+import Footer from '../../Components/Footer/Footer'
 const findpage = () => {
     return (
         <>
-         <Navbar/>
-      <div className={styles.find}>
-         <div className={styles.ihostel}>
-            <img src={hostel}></img>
-        </div>
-        <div className={styles.hoSearch}>
-             <div className={styles.book}>
-               <h1>SELECT HOSTEL</h1>
-             </div>
-          
-            <Drop2down/>
-        </div>
+            <Navbar />
+            <div className={styles.find}>
+                <div className={styles.ihostel}>
+                    <img src={hostel}></img>
+                </div>
+                <div className={styles.hoSearch}>
+                    <div className={styles.book}>
+                        <h1 className={styles.heading}>SELECT HOSTEL</h1>
+                    </div>
 
-         
-        </div>
+                    <Drop2down />
+                </div>
+
+            <Footer />
+            </div>
         </>
     )
 }
