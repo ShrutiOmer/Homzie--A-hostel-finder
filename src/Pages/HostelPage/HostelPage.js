fa0ad439c65971bd67ba62d1da0aa0406ed8322f
Sonali Kushwaha <shonalikushwaha@gmail.com>
2021-10-05 21:40:36 +0530

    gallery layout

--- src/Pages/HostelPage/HostelPage.js
+++ src/Pages/HostelPage/HostelPage.js
@@ -3,13 +3,15 @@ import styles from './HostelPage.module.css'
 import Header from '../../Components/Header/Header'
 import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
+import Gallery from '../../Components/Gallery/Gallery';
 
 const HostelPage = () => {
     return (
         <>
         <Navbar />
         <div className={styles.hostelContainer}>
-        <Header name='Facility'/>
+        <Gallery />
+        <Header name='Facilities'/>
         </div>
         <Footer/>
         </>
