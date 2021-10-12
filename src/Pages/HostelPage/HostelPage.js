c32ecedefa259cca6003c6c93597e172e70ba400
Tanya Bhatnagar <tanubhatt0708.bly@gmail.com>
2021-10-12 22:00:41 +0530

    res

--- src/Pages/HostelPage/HostelPage.js
+++ src/Pages/HostelPage/HostelPage.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import styles from './HostelPage.module.css'
 // import Header from '../../Components/Header/Header'
+import Facility from '../../Components/Facility/Facility'
+import Pricing from '../../Components/Pricing/Pricing'
 import Navbar from "../../Components/Navbar/Navbar";
 import Reviews from "../../Components/Reviews/Reviews";
 import Nearby from "../../Components/Nearby/Nearby"
 import Header from '../../Components/Header/Header'
 import Footer from "../../Components/Footer/Footer";
 import Details from "../../Components/Nearby/Details"
+import Gallery from '../../Components/Gallery/Gallery';
+import Availability from "../../Components/Availability/avail"
 const HostelPage = () => {
     return (
         <>
@@ -29,6 +33,12 @@ const HostelPage = () => {
                         })
                     }
                 </div>
+        <div className={styles.hostelContainer}>
+        <Gallery />
+        <Facility/>
+        <Availability/>
+        <Pricing />
+        </div>
         <Footer/>
         </>
     )
